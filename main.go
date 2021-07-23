727a112db0db9d9fb8125ff3482c3ffa7bb17ecc
João Eliandro Germano <eliandrogermano@gmail.com>
2021-07-23 00:10:42 -0300

    Coming back with learning

--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import "fmt"
 
+var y int
+
 func main() {
 	x := 10
 	y := "Hello World!"
-
-	fmt.Printf("x: %v, %T\n", x, x)
+	result, _ := fmt.Printf("x: %v, %T\n", x, x)
 	fmt.Printf("y: %v, %T\n", y, y)
 
 	x = 20
 	fmt.Printf("x: %v, %T\n", x, x)
+
+	test_func(result)
+}
+
+func test_func(result int) {
+	fmt.Printf("Result: %v\n", result)
+
+	fmt.Println(y)
 }
