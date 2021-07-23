2cc31c376a0cc9670f5ddb4c14b47d06dfcf45b1
João Eliandro Germano <eliandrogermano@gmail.com>
2021-07-23 00:11:06 -0300

    Added basic show values exercise

---
+++ exercises/show_values.go
@@ -0,0 +1,15 @@
+package exercises
+
+import "fmt"
+
+func show_values() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Printf("x: %v, y: %v, z: %v", x, y, z)
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
